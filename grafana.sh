921693ad342178b23b52f8607182b81ee9e785b0
Franco Geraci <30750977+fgeraci-devops-zen@users.noreply.github.com>
2018-07-04 21:26:24 +0200

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,13 @@
+# Add the following to a new file at /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packagecloud.io/grafana/stable/el/7/$basearch
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+
+sudo yum install grafana
+
